6de5e6894ef49fda537a86fbbc70d0ab0021aa4d
William Coscodai <will25felipe@gmail.com>
2026-06-23 10:17:37 -0300

    fix: http service app name

--- services/main.py
+++ services/main.py
@@ -2,19 +2,19 @@ from fastapi import FastAPI
 from services.params import swagger_version
 from internal.main import get_enums, get_modules, get_paths
 
-http_app = FastAPI()
+app = FastAPI()
 
-@http_app.get("/{version}/enums", operation_id="get_enums", description="Get all enums defined in the Swagger JSON for the specified version.")
+@app.get("/{version}/enums", operation_id="get_enums", description="Get all enums defined in the Swagger JSON for the specified version.")
 async def get_enums_endpoint(version: swagger_version):
     enums = get_enums(version)
     return enums
 
-@http_app.get("/{version}/paths/{module_name}", operation_id="get_paths_by_module", description="Get all paths for a specified module in the Swagger JSON for the specified version.")
+@app.get("/{version}/paths/{module_name}", operation_id="get_paths_by_module", description="Get all paths for a specified module in the Swagger JSON for the specified version.")
 async def get_paths_endpoint(version: swagger_version, module_name: str):
     paths = get_paths(version, module_name)
     return paths
 
-@http_app.get("/{version}/modules", operation_id="get_all_modules", description="Get all modules defined in the Swagger JSON for the specified version.")
+@app.get("/{version}/modules", operation_id="get_all_modules", description="Get all modules defined in the Swagger JSON for the specified version.")
 async def get_modules_endpoint(version: swagger_version):
     modules = get_modules(version)
     return modules
--- services/mcp.py
+++ services/mcp.py
@@ -1,9 +1,9 @@
 from fastmcp import FastMCP
 from fastapi import FastAPI
 
-from services.main import http_app
+from services.main import app
 
-mcp = FastMCP.from_fastapi(app=http_app, name="swagger_parser", instructions="This MCP provides an API to parse Swagger JSON files and extract information about enums, paths, and modules.")
+mcp = FastMCP.from_fastapi(app=app, name="swagger_parser", instructions="This MCP provides an API to parse Swagger JSON files and extract information about enums, paths, and modules.")
 
 mcp_app = mcp.http_app(path="/mcp")
 
@@ -13,7 +13,7 @@ combined_app = FastAPI(
   version="0.2",
   routes=[
     *mcp_app.routes,
-    *http_app.routes
+    *app.routes
   ],
   lifespan=mcp_app.lifespan
 )
